59c98734c123ae6955397107866fe2efd6c06ca1
Sampath Bandla <sampathbandla@lewisu.edu>
2022-10-08 14:16:46 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import "./styles/main.scss"
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     
+    </div>
+  )
+}
+
+export default App
